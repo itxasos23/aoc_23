79a584300bb97bac0f29221d22b2bc28ec186220
itxasos23 <txas@txas.me>
2023-11-30 20:02:10 +0100

    boilerplate: update .gitignore and add python boilerplate

---
+++ python/days/day_01/day_01.py
@@ -0,0 +1,17 @@
+import sys 
+from loguru import logger
+
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
+
+
+def part_1():
+    print("part_1")
+
+
+def part_2():
+    print("part_2")
+
+
+def day():
+    part_1()
+    part_2()
